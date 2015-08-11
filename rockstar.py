e2faa9a89c123a79b8a979a220017827fbf50e84
Chaichalit Srisawat <boywert@gmail.com>
2015-08-11 01:38:56 +0100

    quick commit

--- rockstar.py
+++ rockstar.py
@@ -4,10 +4,10 @@ def read_log10mass(file):
     mass = []
     index = f.readline().split().index("M200c_all")
     print "Looking at column",index
-    with open(file,"r") as f
-    for line in f:
-        if line[0] != "#":
-            mass.append(float(line.split()[index]))
+    with open(file,"r") as f:
+        for line in f:
+            if line[0] != "#":
+                mass.append(float(line.split()[index]))
     mass = numpy.array(mass)
     return numpy.log10(mass)
 
