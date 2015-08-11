ca33441679d87e19b54ed87c2dc8fb63a8e9cfce
Chaichalit Srisawat <boywert@gmail.com>
2015-08-11 02:40:20 +0100

    quick commit

--- massfunc.py
+++ massfunc.py
@@ -11,7 +11,7 @@ from global_values import *
 def do_snap(i,z,flist):
     print "Doing snapshot",i,"z =",z[i],flist[i]
     mf_theory_behroozi = hmf.MassFunction(z=z[i],Mmin=M_min,Mmax=M_max,delta_wrt="crit",mf_fit='Behroozi')
-    print mf_theory_behroozi.dndlog10m/112.**3
+    print mf_theory_behroozi.dndlog10m
     mf_theory_watson = hmf.MassFunction(z=z[i],Mmin=M_min,Mmax=M_max,delta_wrt="crit",mf_fit='Watson')
     logm = rockstar.read_log10mass(flist[i])
     hist = numpy.histogram(logm,bins=Nbins,range=(M_min,M_max))
@@ -27,8 +27,8 @@ def do_snap(i,z,flist):
     fig = plot.figure()
     ax = fig.add_subplot(111)
     ax.plot(hist_x,hist_y,label="Rockstar")
-    ax.plot(numpy.log10(mf_theory_watson.M),mf_theory_watson.dndlog10m/112.**3,label="Watson et al. (2012)")
-    ax.plot(numpy.log10(mf_theory_behroozi.M),mf_theory_behroozi.dndlog10m/112.**3,label="Behroozi et al. (2012)")
+    ax.plot(numpy.log10(mf_theory_watson.M),mf_theory_watson.dndlog10m,label="Watson et al. (2012)")
+    ax.plot(numpy.log10(mf_theory_behroozi.M),mf_theory_behroozi.dndlog10m,label="Behroozi et al. (2012)")
     ax.set_yscale("log")
     leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
     fig.savefig("hmf_"+str(z[i])+".pdf")
