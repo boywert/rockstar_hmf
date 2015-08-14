5a7fb44666f7b350f52c1b838a895be0effca842
Chaichalit Srisawat <boywert@gmail.com>
2015-08-14 05:51:40 +0100

    quick commit

--- massfunc.py
+++ massfunc.py
@@ -25,7 +25,7 @@ def do_snap(ii,z,flist):
     hist_y = []
     hist_x = []
     for i in range(Nbins):
-        hist_x.append(0.5*(hist[1][i]+hist[1][i]))
+        hist_x.append(hist[1][i])
         hist_y.append(hist[0][i])
     hist_y = (numpy.array(hist_y,dtype=numpy.float64))/boxsize**3/delta
     plot.rc('text', usetex=True)
