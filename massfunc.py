d6be780e24d80d796dc7c43e8cd444a4d08bcf74
Chaichalit Srisawat <boywert@gmail.com>
2015-08-14 02:38:41 +0100

    quick commit

--- massfunc.py
+++ massfunc.py
@@ -22,20 +22,20 @@ def do_snap(ii,z,flist):
     mf_theory_smt = hmf.MassFunction(dlog10m = delta,z=z[ii],Mmin=M_min,Mmax=M_max,delta_wrt="crit",mf_fit='SMT')
     mf_theory_behroozi = hmf.MassFunction(dlog10m = delta,z=z[ii],Mmin=M_min,Mmax=M_max,delta_wrt="crit",mf_fit='Behroozi')
     mf_theory_watson = hmf.MassFunction(dlog10m = delta,z=z[ii],Mmin=M_min,Mmax=M_max,delta_wrt="crit",mf_fit='Watson')
-    hist_y = []
-    hist_x = []
-    for i in range(Nbins):
-        hist_x.append(0.5*(hist[1][i]+hist[1][i]))
-        hist_y.append(hist[0][i])
+    hist_y = hist[0]
+    hist_x = hist[1]
+    # for i in range(Nbins):
+    #     hist_x.append(0.5*(hist[1][i]+hist[1][i]))
+    #     hist_y.append(hist[0][i])
     hist_y = (numpy.array(hist_y,dtype=numpy.float64))/112.**3/delta
     plot.rc('text', usetex=True)
     fig = plot.figure()
     ax = fig.add_subplot(111)
     ax.plot(hist_x,hist_y,label="Rockstar")
-    ax.plot(mf_theory_ps.,mf_theory_ps.dndlog10m,label="PS")
-    ax.plot(mf_theory_smt.M,mf_theory_smt.dndlog10m,label="SMT")
-    ax.plot(mf_theory_watson.M,mf_theory_watson.dndlog10m,label="Watson et al. (2012)")
-    ax.plot(mf_theory_behroozi.M,mf_theory_behroozi.dndlog10m,label="Behroozi et al. (2012)")
+    ax.plot(numpy.log10(mf_theory_ps.M),mf_theory_ps.dndlog10m,label="PS")
+    ax.plot(numpy.log10(mf_theory_smt.M),mf_theory_smt.dndlog10m,label="SMT")
+    ax.plot(numpy.log10(mf_theory_watson.M),mf_theory_watson.dndlog10m,label="Watson et al. (2012)")
+    ax.plot(numpy.log10(mf_theory_behroozi.M),mf_theory_behroozi.dndlog10m,label="Behroozi et al. (2012)")
     leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
     ax.set_yscale("log")
     ax.text(8.5, 1.e-5, r'$z = '+str(z[ii])+'$')
